972b183b5024d5ab03e504f6087fcbcf0348586f
starbucksdolcelatte <seoyoon9535@gmail.com>
2018-11-20 23:03:45 +0900

    Initialize Fine Today

---
+++ FineToday/app/src/main/java/com/example/seoyoon/finetoday/BasicInfo.java
@@ -0,0 +1,13 @@
+package com.example.seoyoon.finetoday;
+
+public class BasicInfo {
+
+
+    /**
+     * 사진 저장 위치
+     */
+    public static final String FOLDER_PHOTO 		= "/sdcard/FineToday/photo/";
+
+
+
+}
